8bc3f8b828fc1fafb0cfd5771ad318fe2f008110
Erasmo J.F Da Silva <30526854+Erasmojf@users.noreply.github.com>
2022-06-02 16:44:25 -0300

    Ajustes

--- Hacker News/Hacker News/Views/DetailView.swift
+++ Hacker News/Hacker News/Views/DetailView.swift
@@ -3,7 +3,9 @@
 //  Hacker News
 //
 //  Created by Erasmo J.F Da Silva on 02/06/22.
-//
+//  SwiftUI ♡ Better Apps. Less Code
+//  https://erasmojf.github.io/
+//  Fidju de Bideira de Feira de Caracol
 
 import SwiftUI
 
