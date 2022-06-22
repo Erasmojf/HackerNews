702e3fa330dfa7a5dcaf58f0316b067eeb2f1dac
Erasmo J.F Da Silva <30526854+Erasmojf@users.noreply.github.com>
2022-06-22 11:16:26 -0300

    Ajuste

--- Hacker News/Hacker News/Models/PostData.swift
+++ Hacker News/Hacker News/Models/PostData.swift
@@ -3,7 +3,9 @@
 //  Hacker News
 //
 //  Created by Erasmo J.F Da Silva on 26/05/22.
-//
+//  SwiftUI ♡ Better Apps. Less Code
+//  https://erasmojf.github.io/
+//  Fidju de Bideira de Feira de Caracol
 
 import Foundation
 
